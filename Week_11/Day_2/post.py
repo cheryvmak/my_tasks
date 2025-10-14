b0ea201d222a72165849626d63d7de5c70c6db6f
sherif <cheryvmak.cs@gmail.com>
2025-10-14 16:03:47 +0100

    Week 11

--- Week_11/Day_2/post.py
+++ Week_11/Day_2/post.py
@@ -7,24 +7,23 @@ data = []
 class BasicAPI(BaseHTTPRequestHandler):
     def send_data(self, payload, status = 201):
         self.send_response(status)
-        self.send_header("content-Type", "application/json")
+        self.send_header("Content-Type", "application/json")
         self.end_headers()
         self.wfile.write(json.dumps(payload).encode())
 
     def do_POST(self):
-        content_size = int(self.headers.get("COntent-Length", 0))
-        self.rfile.read(content_size)
+        content_size = int(self.headers["COntent-Length"], 0)
         parsed_data = self.rfile.read(content_size)
-
         post_data = json.loads(parsed_data)
         data.append(post_data) #saving to a database
+
         self.send_data({
             "Message":"Data Received",
             "data": post_data
         })
 
 def run():
-        HTTPServer(('localhost', 9000), BasicAPI).serve_forever()
+        HTTPServer(('localhost', 5000), BasicAPI).serve_forever()
 
 print("Application is running")
 run()
