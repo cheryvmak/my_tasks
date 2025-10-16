77cc2af2366d6c4e12676fc50ff80540e41f1ecf
Sherif <cheryvmak.cs@gmail.com>
2025-10-16 13:16:28 +0100

    Week 11 FastAPI

---
+++ Week_11/Day_4/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import uvicorn
+import os   
+
+load_dotenv()
+
+app = FastAPI(title="Simple FAastAPI App", version="1.0.0")
+
+@app.get("/")
+def root():
+    return{"Message":"Welcome to my FastAPI Application"}
+
+if __name__ == "__main__":
+    print(os.getenv("host"))
+    print(os.getenv("port"))
+    
+    uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port")))
+
+
+    # uvicorn.run(
+    # app,
+    # host=os.getenv("host", "127.0.0.1"),
+    # port=os.getenv("port", 8000))
+
